caf2617874345826107ed19392ad8cbc4ce955af
PeterHiroshi <peter8icestone@gmail.com>
2018-07-19 16:44:59 +0800

    Initial Commit

---
+++ SearchKingEatingPath/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SearchKingEatingPath
+//
+//  Created by 清水ヒロシ on 2018/07/19.
+//  Copyright © 2018年 ice-stone. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
